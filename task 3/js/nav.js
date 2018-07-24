77dbf4db587ba24a8f49d04d8c2e57fb10a5588d
Arjun-Gopinath <arjungopinath06@gmail.com>
2018-07-24 20:53:40 +0530

    edit

--- task 3/js/nav.js
+++ task 3/js/nav.js
@@ -1,14 +1,16 @@
 $(window).scroll(function(){
-if ($(document).scrollTop()>50){
-$('nav').addClass('shrink');
-$('.dropdown').css({"padding-top":"8px"});
-$('.navbar-brand img').css({"height":"35px"});
-$('.navbar-color').css({"background-color":"rgba(0,0,0,.5)"});
-}
-else{
-$('nav').removeClass('shrink');
-$('.dropdown').css({"padding-top":"30px"});
-$('.navbar-brand img').css({"height":"80px"});
-$('.navbar-color').css({"background-color":"#222"});
-}
+    if ($(document).scrollTop()>50){
+    $('nav').addClass('shrink');
+    $('.dropdown').css({"padding-top":"8px"});
+    $('.navbar-brand img').css({"height":"45px"});
+    $('.navbar-brand img').css({"width":"120px"});
+    $('.navbar-color').css({"background-color":"rgba(0,0,0,.8)"});
+    }
+    else{
+    $('nav').removeClass('shrink');
+    $('.dropdown').css({"padding-top":"30px"});
+    $('.navbar-brand img').css({"height":"80px"});
+        $('.navbar-brand img').css({"width":"200px"});
+    $('.navbar-color').css({"background-color":"#222"});
+    }
 });
