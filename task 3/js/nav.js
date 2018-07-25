141929a3a79f856eca1573055638b7e0e1215a00
Arjun-Gopinath <arjungopinath06@gmail.com>
2018-07-25 10:02:36 +0530

    main page

--- task 3/js/nav.js
+++ task 3/js/nav.js
@@ -2,15 +2,15 @@ $(window).scroll(function(){
     if ($(document).scrollTop()>50){
     $('nav').addClass('shrink');
     $('.dropdown').css({"padding-top":"8px"});
-    $('.navbar-brand img').css({"height":"45px"});
-    $('.navbar-brand img').css({"width":"120px"});
+    $('.navbar-brand img').css({"height":"30px"});
+    $('.navbar-brand img').css({"width":"80px"});
     $('.navbar-color').css({"background-color":"rgba(0,0,0,.8)"});
     }
     else{
     $('nav').removeClass('shrink');
     $('.dropdown').css({"padding-top":"30px"});
-    $('.navbar-brand img').css({"height":"80px"});
-        $('.navbar-brand img').css({"width":"200px"});
+    $('.navbar-brand img').css({"height":"40px"});
+        $('.navbar-brand img').css({"width":"100px"});
     $('.navbar-color').css({"background-color":"#222"});
     }
 });
